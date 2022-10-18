54665153bbee96579916ac68feef143f3ffdfbe5
20ioppolan <20ioppolan@gmail.com>
2022-10-18 17:16:19 -0400

    oh lord

--- MANgle.py
+++ MANgle.py
@@ -3,23 +3,33 @@ import random
 import subprocess
 
 ROOTS = [f"man{i}" for i in range(1,9)]
+PATH = "/usr/share/man/"
+
+def makeFileList(path):
+    manList = list()
+    for root, dirs, files in os.walk(f"{path}", topdown=True):
+            rootpath = root.split("/")
+            if rootpath[4] in ROOTS:
+                for name in files:
+                    manList.append(os.path.join(root,name))
+    return manList
+
 
 def main():
-    manList = list()
+    manList = makeFileList(PATH)
     mixedManList = list()
-    # subprocess.Popen("cp -r /usr/share/man/man8 /usr/share/man/man8backup",shell=True)
-    for root, dirs, files in os.walk(f"/usr/share/man/", topdown=True):
-        rootpath = root.split("/")
-        if rootpath[4] in ROOTS:
-            for name in files:
-                # print(os.path.join(root, name))
-                manList.append(os.path.join(root,name))
-                mixedManList.append(os.path.join(root,name))
+    # subprocess.Popen("cp -r /usr/share/man/man{1..8} /usr/share/man/manBackup",shell=True)
+    subprocess.Popen("cp -r man{1..8} MAN_PAGE_BACKUP", shell=True)
+    # subprocess.Popen("rm -rf man{1..8}", shell=True)
+    for file in manList:
+        subprocess.Popen(f"cp {file} man1", shell=True)
+        
+    
 
     
-    # for i in range(2000):
-    #     # print(manList[i])
-    #     print(manList[random.randint(0,len(manList)-1)])
+    for i in range(2000):
+        # print(manList[i])
+        print(manList[random.randint(0,len(manList)-1)])
 
 
 if __name__ == "__main__":
