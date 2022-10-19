2df5cac8338c08af5015db0e12f4b79c0131487a
Anthony Ioppolo <20ioppolan@gmail.com>
2022-10-19 11:45:32 -0400

    cleaning code

--- MANgle.py
+++ MANgle.py
@@ -1,11 +1,11 @@
 import os
 import random
-import subprocess
-import time
+
 
 ROOTS = [f"man{i}" for i in range(1,9)]
 PATH = "/usr/share/man/"
 
+# Make a list of all man page files
 def makeFileList(path):
     manList = list()
     for root, dirs, files in os.walk(f"{path}", topdown=True):
@@ -15,10 +15,12 @@ def makeFileList(path):
                     manList.append(os.path.join(root,name))
     return manList
 
+# Removes the original man pages to prevent indexing
 def deleteManFiles():
     for i in range(1,10):
         os.system(f"rm -rf /usr/share/man/man{i}")
 
+# Backup the original man pages because I'm not that evil 
 def makeManBackup():
     for i in range(1,10):
         os.system(f"cp -rf /usr/share/man/man{i} /usr/share/man/MAN_PAGE_BACKUP")
@@ -26,22 +28,16 @@ def makeManBackup():
 
 def main():
     manList = makeFileList(PATH)
-    # subprocess.Popen("cp -r /usr/share/man/man{1..8} /usr/share/man/manBackup",shell=True)
     os.system("mkdir /usr/share/man/MAN_PAGE_BACKUP")
     makeManBackup()
     os.system("mkdir /usr/share/man/temp")
-    # subprocess.Popen("rm -rf man{1..8}", shell=True)
     
     os.system("mkdir /usr/share/man/hell")
-    # randomize list
     random.shuffle(manList) 
-    # store files in temp folder
     for file in manList:
         os.system(f"cp {file} /usr/share/man/temp")
-    # copy list[i] -> replacement folder
     print("[DEBUG] Manlist Size:", len(manList))
     for i in range(len(manList)):
-        
         
         if i == len(manList)-1:
             file = manList[i].rsplit('/', 1)[-1]
@@ -53,18 +49,11 @@ def main():
             nextfile = manList[i+1].rsplit('/', 1)[-1]
             print(f"[DEBUG] Copying Value {i} into {i+1}")
             os.system(f"cp /usr/share/man/temp/{file} /usr/share/man/hell/{nextfile}")
-    # os.system("rm -rf /usr/share/man/temp")
 
     deleteManFiles()
 
     os.system("cp -rf /usr/share/man/hell /usr/share/man/man1")
-    print(manList[i])
-    # ball 
-
-
-    # for i in range(2000):
-    #     # print(manList[i])
-    #     print(manList[random.randint(0,len(manList)-1)])
+   
 
 
 if __name__ == "__main__":
