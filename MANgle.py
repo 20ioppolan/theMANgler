a6df984db013227e6afd40329a69cc1e8dad9d39
20ioppolan <20ioppolan@gmail.com>
2022-10-19 10:40:41 -0400

    maybe working?

--- MANgle.py
+++ MANgle.py
@@ -1,6 +1,7 @@
 import os
 import random
 import subprocess
+import time
 
 ROOTS = [f"man{i}" for i in range(1,9)]
 PATH = "/usr/share/man/"
@@ -14,34 +15,46 @@ def makeFileList(path):
                     manList.append(os.path.join(root,name))
     return manList
 
+def makeManBackup():
+    for i in range(1,10):
+        os.system(f"cp -rf /usr/share/man/man{i} /usr/share/man/MAN_PAGE_BACKUP")
+
 
 def main():
     manList = makeFileList(PATH)
-    shuffledManList = list()
     # subprocess.Popen("cp -r /usr/share/man/man{1..8} /usr/share/man/manBackup",shell=True)
-    os.system("mkdir /usr/share/man/MAN_PAGE_BACKUP;cp -rf /usr/share/man/man{1..8} /usr/share/man/MAN_PAGE_BACKUP;mkdir /usr/share/man/temp")
+    os.system("mkdir /usr/share/man/MAN_PAGE_BACKUP")
+    makeManBackup()
+    os.system("mkdir /usr/share/man/temp")
     # subprocess.Popen("rm -rf man{1..8}", shell=True)
     
-        
-    
-    os.system("mkdir usr/share/man/hell")
+    os.system("mkdir /usr/share/man/hell")
     # randomize list
-    shuffledManList = random.shuffle(manList) 
+    random.shuffle(manList) 
     # store files in temp folder
     for file in manList:
         os.system(f"cp {file} /usr/share/man/temp")
     # copy list[i] -> replacement folder
-    for i in range(len(shuffledManList)):
-        if i == len(shuffledManList):
-            os.system(f"cp /usr/share/man/temp/{shuffledManList[i]} /usr/share/man/hell/{shuffledManList[0]}")
+    print("[DEBUG] Manlist Size:", len(manList))
+    for i in range(len(manList)):
+        
+        
+        if i == len(manList)-1:
+            file = manList[i].rsplit('/', 1)[-1]
+            nextfile = manList[0].rsplit('/', 1)[-1]
+            print(f"[DEBUG] Copying Value {i} into {0}")
+            os.system(f"cp {file} /usr/share/man/hell/{nextfile}")
         else:
-            os.system(f"cp /usr/share/man/temp/{shuffledManList[i]} /usr/share/man/hell/{shuffledManList[i+1]}")
-    # delete from temp folder
-    os.system("rm -rf /usr/share/man/temp")
-    # move to man 1 (or disperse around 9)
-    os.system("rm -rf /usr/share/man/man{1..8}")
-    os.system("cp -rf /usr/share/man/hell /usr/share/man/man1")
+            file = manList[i].rsplit('/', 1)[-1]
+            nextfile = manList[i+1].rsplit('/', 1)[-1]
+            print(f"[DEBUG] Copying Value {i} into {i+1}")
+            os.system(f"cp {file} /usr/share/man/hell/{nextfile}")
+    # os.system("rm -rf /usr/share/man/temp")
 
+    os.system("rm -rf /usr/share/man/man{1..8}")
+
+    os.system("cp -rf /usr/share/man/hell /usr/share/man/man1")
+    print(manList[i])
     # ball 
 
 
