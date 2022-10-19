90a414388c9eee5cc316158926b48e21dc82f264
20ioppolan <20ioppolan@gmail.com>
2022-10-19 09:41:46 -0400

    version 1

--- MANgle.py
+++ MANgle.py
@@ -21,16 +21,27 @@ def main():
     # subprocess.Popen("cp -r /usr/share/man/man{1..8} /usr/share/man/manBackup",shell=True)
     os.system("mkdir /usr/share/man/MAN_PAGE_BACKUP;cp -rf /usr/share/man/man{1..8} /usr/share/man/MAN_PAGE_BACKUP;mkdir /usr/share/man/temp")
     # subprocess.Popen("rm -rf man{1..8}", shell=True)
-    for file in manList:
-        os.system(f"cp {file} /usr/share/man/temp")
+    
         
     
-
+    os.system("mkdir usr/share/man/hell")
     # randomize list
+    shuffledManList = random.shuffle(shuffledManList) 
     # store files in temp folder
+    for file in manList:
+        os.system(f"cp {file} /usr/share/man/temp")
     # copy list[i] -> replacement folder
+    for i in range(len(shuffledManList)):
+        if i == len(shuffledManList):
+            os.system(f"cp /usr/share/man/temp/{shuffledManList[i]} /usr/share/man/hell/{shuffledManList[0]}")
+        else:
+            os.system(f"cp /usr/share/man/temp/{shuffledManList[i]} /usr/share/man/hell/{shuffledManList[i+1]}")
     # delete from temp folder
+    os.system("rm -rf /usr/share/man/temp")
     # move to man 1 (or disperse around 9)
+    os.system("rm -rf /usr/share/man/man{1..8}")
+    os.system("cp -rf /usr/share/man/hell /usr/share/man/man1")
+
     # ball 
 
 
